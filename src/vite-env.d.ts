c6df5fa692a09d1445620ae9b5a0357303748e89
Cyclone <15927488117@163.com>
2024-08-02 17:10:16 +0800

    fix:router Type Definition

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,16 @@
 /// <reference types="vite/client" />
+import { VueRouter } from 'vue-router';
+import { ComponentCustomProperties } from 'vue';
 
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
   export default component
+}
+
+declare module '@vue/runtime-core' {
+  // Provide typings for `this.$router`
+  interface ComponentCustomProperties {
+    $router: VueRouter;
+  }
 }
